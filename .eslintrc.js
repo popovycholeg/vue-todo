601cd5901723e4d9a48f2d7dde2efdb6ec9586bb
Oleg Popovych <magmoba@gmail.com>
2020-09-15 00:47:07 +0300

    linter settings & code formating

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,26 @@ module.exports = {
     // this section will be used to determine which APIs are available to us
     // (i.e are we running in a browser environment or a node.js env)
     node: true,
-    browser: true
+    browser: true,
   },
   parserOptions: {
     parser: "babel-eslint",
     // specifying a module sourcetype prevent eslint from marking import statements as errors
-    sourceType: "module"
+    sourceType: "module",
   },
   extends: [
     // use the recommended rule set for both plain javascript and vue
     "eslint:recommended",
-    "plugin:vue/recommended"
+    "plugin:vue/recommended",
   ],
   rules: {
     // we should always disable console logs and debugging in production
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  }
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+  html: {
+    void: "always",
+    normal: "never",
+    component: "any",
+  },
 };
