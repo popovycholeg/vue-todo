9a018be05375c94db9d4c21ada9b060288536e0c
Oleg Popovych <magmoba@gmail.com>
2020-09-15 23:48:45 +0300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,11 @@
 module.exports = {
-  html: {
-    void: "never",
-    normal: "always",
-    component: "always",
-  },
-  svg: "always",
-  math: "always",
-  root: true,
-  env: {
-    // this section will be used to determine which APIs are available to us
-    // (i.e are we running in a browser environment or a node.js env)
-    node: true,
-    browser: true,
-  },
-  parserOptions: {
-    parser: "babel-eslint",
-    // specifying a module sourcetype prevent eslint from marking import statements as errors
-    sourceType: "module",
-  },
   extends: [
-    // use the recommended rule set for both plain javascript and vue
-    "eslint:recommended",
-    "plugin:vue/recommended",
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    "plugin:vue/vue3-recommended",
   ],
   rules: {
-    // we should always disable console logs and debugging in production
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
   },
 };
