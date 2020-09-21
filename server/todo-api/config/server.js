f5cb9df230b2ad09650e72b5c11293977fc2611d
Oleg Popovych <magmoba@gmail.com>
2020-09-21 23:12:04 +0300

    Setup strapi

---
+++ server/todo-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c5293ab39878ecb6a34e9a67d68f7cef'),
+    },
+  },
+});
---
+++ server/todo-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a9303739-df0d-4579-b470-c1339aff7db8'
+};
