f1a759100f73834ea7b4bb2260ee0a4932674d5f
Oleh I Popovych <opop1@softserveinc.com>
2021-03-06 21:34:13 +0200

    Move creds to .env

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { TodosModule } from './todos/todos.module';
+import { CONNECTION_STRING } from './config/db';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(CONNECTION_STRING),
     TodosModule,
-    MongooseModule.forRoot(
-      // TODO: move to config
-      'mongodb+srv://oleh:q12345678@cluster0.2magi.azure.mongodb.net/todosApp?retryWrites=true&w=majority',
-    ),
   ],
   controllers: [AppController],
   providers: [],
