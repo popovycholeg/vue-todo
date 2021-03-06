67f6262b4b17ce8db8ec4dcf31c2b83b96117d91
Oleh I Popovych <opop1@softserveinc.com>
2021-03-06 20:21:23 +0200

    get all todos

--- nest-server/src/todos/todos.service.ts
+++ nest-server/src/todos/todos.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Todo, TodoDocument } from './schemes/todos.schema';
 
 @Injectable()
 export class TodosService {
-  getAllTodos(): string {
-    return 'all';
+  constructor(@InjectModel(Todo.name) private todoModel: Model<TodoDocument>) {}
+
+  async getAllTodos(): Promise<Todo[]> {
+    return this.todoModel.find().exec();
   }
 }
