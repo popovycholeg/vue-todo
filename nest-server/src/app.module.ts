da6c72b80f4bf3a9229d0d4919c0ff2517af85e9
Oleh I Popovych <opop1@softserveinc.com>
2021-03-06 20:16:48 +0200

    Add todo schema

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [TodosModule],
+  imports: [
+    TodosModule,
+    MongooseModule.forRoot(
+      // TODO: move to config
+      'mongodb+srv://oleh:q12345678@cluster0.2magi.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [],
 })
