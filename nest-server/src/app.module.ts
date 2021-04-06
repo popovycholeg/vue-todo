6680452d7760336f585b8e595caafb33fb19767e
Oleh Popovych <opop1@softserveinc.com>
2021-04-06 20:32:00 +0300

    Added packages & create structure

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import { TodosModule } from './todos/todos.module';
       `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_PROVIDER}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     ),
     TodosModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [],
